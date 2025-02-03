56a7fd2320d87d664391cb1384b293b5c4915c6e
nagetpresto <bilqistaz27@gmail.com>
2025-02-03 17:06:50 +0700

    add console log

--- DemoKCIC/obj/Debug/net8.0/DemoKCIC.AssemblyInfo.cs
+++ DemoKCIC/obj/Debug/net8.0/DemoKCIC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoKCIC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5663f95d47d3fa16cd8dfe7177499b0365c346d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfd096501bf440ed3cb82ca8e5bdc9f2b8f6ed55")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoKCIC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoKCIC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
